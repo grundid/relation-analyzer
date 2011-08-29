9083621566fa00fd01885f1dbf9d51641444ea11
Adrian Stabiszewski <as@grundid.de>
2011-08-29 10:14:05 +0200

    license added

--- src/main/java/org/osmsurround/ra/data/Node.java
+++ src/main/java/org/osmsurround/ra/data/Node.java
@@ -1,3 +1,20 @@
+/**
+ * This file is part of Relation Analyzer for OSM.
+ * Copyright (c) 2001 by Adrian Stabiszewski, as@grundid.de
+ *
+ * Relation Analyzer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Relation Analyzer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Relation Analyzer.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.osmsurround.ra.data;
 
 import java.io.Serializable;
