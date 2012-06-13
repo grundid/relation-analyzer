246f9fa8b5fc6e88b077b056a47d98fd87caa2f3
Adrian Stabiszewski <as@grundid.de>
2012-06-13 08:32:18 +0200

    elevation service init changed

--- src/main/java/org/osmsurround/ra/elevation/ElevationService.java
+++ src/main/java/org/osmsurround/ra/elevation/ElevationService.java
@@ -11,13 +11,12 @@ import org.osmsurround.ra.utils.LonLatMath;
 import org.osmtools.srtm.SrtmService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ElevationService implements InitializingBean {
+public class ElevationService {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
 
@@ -29,42 +28,48 @@ public class ElevationService implements InitializingBean {
 
 	private SrtmService srtmService;
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		srtmService = new SrtmService();
-		if (srtmDataDirectory != null) {
-			log.info("Scanning dir for Srtm Data... " + srtmDataDirectory);
-			srtmService.scanDirectory(srtmDataDirectory);
+	public void assertSrtmService() {
+		if (srtmService == null) {
+			srtmService = new SrtmService();
+			if (srtmDataDirectory != null) {
+				log.info("Scanning dir for Srtm Data... " + srtmDataDirectory);
+				srtmService.scanDirectory(srtmDataDirectory);
+			}
 		}
 	}
 
 	public List<double[]> createElevationProfile(AnalyzerContext context) {
+		assertSrtmService();
 		List<double[]> list = new ArrayList<double[]>();
-		List<Section> sections = exportService.convertToSections(context);
-		if (!sections.isEmpty()) {
+		try {
+			List<Section> sections = exportService.convertToSections(context);
+			if (!sections.isEmpty()) {
 
-			Section section = sections.get(0);
-			if (!section.getCoordinateLists().isEmpty()) {
+				Section section = sections.get(0);
+				if (!section.getCoordinateLists().isEmpty()) {
 
-				double length = 0;
-				LonLat lastLonLat = null;
+					double length = 0;
+					LonLat lastLonLat = null;
 
-				for (LonLat lonLat : section.getCoordinateLists().iterator().next()) {
+					for (LonLat lonLat : section.getCoordinateLists().iterator().next()) {
 
-					double lon = lonLat.getLon();
-					double lat = lonLat.getLat();
+						double lon = lonLat.getLon();
+						double lat = lonLat.getLat();
 
-					int height = srtmService.getElevation(lon, lat);
+						int height = srtmService.getElevation(lon, lat);
 
-					if (lastLonLat != null) {
-						double distance = LonLatMath.distance(lastLonLat.getLon(), lastLonLat.getLat(),
-								lonLat.getLon(), lonLat.getLat());
-						length += distance;
-						list.add(new double[] { length, height });
+						if (lastLonLat != null) {
+							double distance = LonLatMath.distance(lastLonLat.getLon(), lastLonLat.getLat(),
+									lonLat.getLon(), lonLat.getLat());
+							length += distance;
+							list.add(new double[] { length, height });
+						}
+						lastLonLat = lonLat;
 					}
-					lastLonLat = lonLat;
 				}
 			}
+		}
+		catch (Exception e) {
 		}
 		return list;
 	}
