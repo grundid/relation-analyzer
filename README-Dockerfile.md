8e53cf30465bab95f2a77b2a77712573174cb88a
Wiktor Niesiobedzki <github@vink.pl>
2017-03-23 12:57:43 +0100

    Use system properties to override config.
    * use system properties / CATALINA_OPTS to pass options to Relation Analyzer
    * rename host postgres to db in docker-compose

--- README-Dockerfile.md
+++ README-Dockerfile.md
@@ -7,8 +7,7 @@ This image is based on Tomcat image and does not contain any database. For full
 functionality (caching, search) database is required. This image supports
 PostgreSQL databases. To setup the database provide following environment
 variables:
-* `DB_HOST` - hostname of the database, defaults to: postgres
-* `DB_NAME` - database name, defaults to: osm
+* `DB_URL` - database URL in format `jdbc:postgresql://[host]/[database name]`
 * `DB_USER` - database username
 * `DB_PASSWORD` - database password
 * `SRTM_DIRECTORY` - directory where SRTM files are available
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-cat >> /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/config.properties << EOF
-db.driverClassName=org.postgresql.Driver
-db.url=jdbc:postgresql://${DB_HOST:-postgres}/${DB_NAME:-osm}
-db.username=${DB_USER}
-db.password=${DB_PASSWORD}
-srtmDataDirectory=${SRTM_DIRECTORY}
-EOF
+if [ -n "$DB_URL" ] ; then
+    CATALINA_OPTS="${CATALINA_OPTS} -Ddb.url=${DB_URL}"
+fi
 
+if [ -n "${DB_USER}" ] ; then
+    CATALINA_OPTS="${CATALINA_OPTS} -Ddb.username=${DB_USER}"
+fi
+
+if [ -n "${DB_PASSWORD}" ] ; then
+    CATALINA_OPTS="${CATALINA_OPTS} -Ddb.password=${DB_PASSWORD}"
+fi
+
+if [ -n "${SRTM_DIRECTORY}" ] ; then
+    CATALINA_OPTS="${CATALINA_OPTS} -DsrtmDataDirectory=${SRTM_DIRECTORY}"
+fi
+
+export CATALINA_OPTS
 exec catalina.sh run
